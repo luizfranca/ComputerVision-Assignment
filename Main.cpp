5f8dec5426cc00672e67550b100ee0936923d82e
Luiz Daniel Ramos Franca <luizdaniel.r.f@gmail.com>
2017-06-17 14:34:00 -0300

    init project

---
+++ Main.cpp
@@ -0,0 +1,22 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: Jun 17, 2017
+ *      Author: luizdaniel
+ */
+
+
+#include <iostream>
+#include "opencv2/opencv.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main() {
+
+
+	cout << "Assignment" << endl;
+
+	return 0;
+}
+
